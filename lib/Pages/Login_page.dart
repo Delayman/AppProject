ea2f4d88037531d747b4b870385eed39b8bb38b7
Dream023 <61343046+Dream023@users.noreply.github.com>
2023-05-26 14:59:18 +0700

    Revert "Merge branch 'Dream' into Perth"
    This reverts commit 40e9a2262a5208d2d6b1f575f09437668a597982, reversing
    changes made to 5426d645ba67861d607fbeafb63e9c93c72d698e.

--- lib/Pages/Login_page.dart
+++ lib/Pages/Login_page.dart
@@ -5,73 +5,96 @@ import 'package:http/http.dart' as http;
 import 'dart:io';
 import 'dart:convert';
 
-class Loginpage extends StatelessWidget{
+class Item {
+  final int? id;
+  final String? username;
+  final String? password;
+
+  Item({
+    this.id,
+    this.username,
+    this.password,
+  });
+}
+
+class Loginpage extends StatelessWidget {
   const Loginpage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Container(
-          decoration: BoxDecoration(
+          child: Container(
+        decoration: BoxDecoration(
             gradient: LinearGradient(
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-            colors: [Color.fromARGB(255, 0, 0, 57),
-            Color.fromARGB(255, 0, 189, 241)])),
-          child: ListView(
-            children: <Widget>[
-              Container(  //กล่องโลโก้
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [
+              Color.fromARGB(255, 0, 0, 57),
+              Color.fromARGB(255, 0, 189, 241)
+            ])),
+        child: ListView(
+          children: <Widget>[
+            Container(
+                //กล่องโลโก้
                 height: 350, //แก้ความสูงส่วนของช่องใส่โลโก้ตรงนี้
-                padding: const EdgeInsets.only(bottom: 70), //แก้ระยะห่างของโลโก้จากแถบขาวตรงนี้
+                padding: const EdgeInsets.only(
+                    bottom: 70), //แก้ระยะห่างของโลโก้จากแถบขาวตรงนี้
                 child: Align(
-                alignment: Alignment.bottomCenter,
-                child: Image.asset('assets/pic/Logo.png', height: 50) //แก้ Logo กับขนาด Logo ตรงนี้
-                ,)
-              ),
-              Container( //กล่องปุ่มกด
+                  alignment: Alignment.bottomCenter,
+                  child: Image.asset('assets/pic/Logo.png',
+                      height: 50) //แก้ Logo กับขนาด Logo ตรงนี้
+                  ,
+                )),
+            Container(
+                //กล่องปุ่มกด
                 alignment: Alignment.center,
                 padding: const EdgeInsets.only(bottom: 10),
                 height: 300, //แก้ความสูงแถบขาวตรงนี้
                 child: Container(
                   width: double.infinity,
                   alignment: Alignment.center,
-                  padding: const EdgeInsets.only(right: 30,left: 30),
+                  padding: const EdgeInsets.only(right: 30, left: 30),
                   child: ListView(
-                    shrinkWrap: true,
-                    scrollDirection: Axis.vertical,
-                    children: <Widget>[
-                      ElevatedButton(
-                    onPressed: (() {
-                      debugPrint("Sign in!");
-                      //Link function here!
-                      Navigator.push(
-                      context,MaterialPageRoute(builder: (context) => Signin()),);
-                    }),
-                    child: Text("Sign in"),
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.blueAccent,
-                      padding: EdgeInsets.all(20)),
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  ElevatedButton(
-                    onPressed: (() {
-                      debugPrint("Sign up!");
-                      //Link function here!
-                      Navigator.push(
-                      context,MaterialPageRoute(builder: (context) => Signup()),);
-                    }),
-                    child: Text("Sign up"),
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.blueAccent,
-                      padding: EdgeInsets.all(20)),
-                  )
-                    ]),)
-              )
-            ],
-    ),)),
+                      shrinkWrap: true,
+                      scrollDirection: Axis.vertical,
+                      children: <Widget>[
+                        ElevatedButton(
+                          onPressed: (() {
+                            debugPrint("Sign in!");
+                            //Link function here!
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (context) => Signin()),
+                            );
+                          }),
+                          child: Text("Sign in"),
+                          style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.blueAccent,
+                              padding: EdgeInsets.all(20)),
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        ElevatedButton(
+                          onPressed: (() {
+                            debugPrint("Sign up!");
+                            //Link function here!
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (context) => Signup()),
+                            );
+                          }),
+                          child: Text("Sign up"),
+                          style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.blueAccent,
+                              padding: EdgeInsets.all(20)),
+                        )
+                      ]),
+                ))
+          ],
+        ),
+      )),
     );
   }
 }
--- lib/Pages/Sign_in.dart
+++ lib/Pages/Sign_in.dart
@@ -5,124 +5,152 @@ import 'package:http/http.dart' as http;
 import 'dart:io';
 import 'dart:convert';
 
+class Item {
+  final int? id;
+  final String? username;
+  final String? password;
+
+  Item({
+    this.id,
+    this.username,
+    this.password,
+  });
+}
+
+String _localhost() {
+  if (Platform.isAndroid)
+    return 'http://10.0.2.2:3306';
+  else
+    return 'http://localhost:3306';
+}
 
 class Signin extends StatelessWidget {
-  
-  final List<TextEditingController> UserDatalist =
-      List.generate(2, (i) => TextEditingController());
-  final email = '';
-  final password = '';
+  Future? serverResponse;
+  Signin({super.key});
+
+  Future<List<Item>> getData() async {
+    final response = await http.get(Uri.parse(_localhost() + "/showDB"));
+
+    var responseData = json.decode(response.body);
+    //Creating a list to store input data;
+    List<Item> items = [];
+    responseData.forEach((index, value) {
+      Item item =
+          Item(username: value["username"], password: value["password"]);
+      items.add(item);
+    });
+
+    return items;
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color.fromARGB(198, 0, 0, 70),
       body: Center(
-        child: ListView(
+          child: ListView(
         children: <Widget>[
-          Container(  //กล่องโลโก้
-            height: 200, //แก้ความสูงส่วนของช่องใส่โลโก้ตรงนี้
-            padding: const EdgeInsets.only(bottom: 30), //แก้ระยะห่างของโลโก้จากแถบขาวตรงนี้
-            child: Align(
-            alignment: Alignment.bottomCenter,
-            child: Image.asset('assets/pic/Logo.png', height: 50) //แก้ Logo กับขนาด Logo ตรงนี้
-            ,)
-          ),
-          Container( //กล่องปุ่มกด
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(20),
-                topRight: Radius.circular(20))
-            ),
-            alignment: Alignment.center,
-            padding: const EdgeInsets.only(bottom: 10),
-            height: 500, //แก้ความสูงแถบขาวตรงนี้
-            child: Container(
-              padding: const EdgeInsets.only(right: 30,left: 30),
+          Container(
+              //กล่องโลโก้
+              height: 200, //แก้ความสูงส่วนของช่องใส่โลโก้ตรงนี้
+              padding: const EdgeInsets.only(
+                  bottom: 30), //แก้ระยะห่างของโลโก้จากแถบขาวตรงนี้
+              child: Align(
+                alignment: Alignment.bottomCenter,
+                child: Image.asset('assets/pic/Logo.png',
+                    height: 50) //แก้ Logo กับขนาด Logo ตรงนี้
+                ,
+              )),
+          Container(
+              //กล่องปุ่มกด
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(20),
+                      topRight: Radius.circular(20))),
               alignment: Alignment.center,
-              child: ListView(
-                shrinkWrap: true,
-                scrollDirection: Axis.vertical,
-                children: <Widget>[
-                  Text("Sign in",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 40,
-                    fontWeight: FontWeight.bold,)),
-                  SizedBox(height: 20,),
-                  Text("Email",
-                  style: TextStyle(fontSize: 20)),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  TextField(
-                    controller: UserDatalist[0],
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'Email...',
-                      hintText: 'Enter valid email id as abc@gmail.com'
-                    ),
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  Text("Password",
-                  style: TextStyle(fontSize: 20)),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  TextField(
-                    controller: UserDatalist[1],
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'Password...',
-                      hintText: 'Enter password'
-                    ),
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  ElevatedButton(
-                    onPressed: (() {
-                      debugPrint("Sign in!");
-                      if(UserDatalist[0] == email && UserDatalist[1] == password)
-                      {
-                        //ลิงค์ไปหน้า Menu ตรงนี้
-                        Navigator.push(
-                        context,MaterialPageRoute(builder: (context) => const Loginpage()),);
-                      }
-                      else{
-                        showDialog(context: context,
-                         builder: ((context){
-                          return AlertDialog(
-                            content: Text("Incorrect username or password!"));
-                         }));
-                      }
-                    }),
-                    child: Text("Sign in"),
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.blueAccent,
-                      padding: EdgeInsets.all(20)),
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  TextButton(
-                    onPressed: (() {
-                      debugPrint("Go to sign up!");
-                      Navigator.push(
-                      context,MaterialPageRoute(builder: (context) => Signup()),);
-                    }),
-                    child: Text("Sign up",
-                    style: TextStyle(
-                      color: Color.fromARGB(255, 0, 46, 84),
-                    )),
-                  )
-                    ]),)
-          )
+              padding: const EdgeInsets.only(bottom: 10),
+              height: 500, //แก้ความสูงแถบขาวตรงนี้
+              child: Container(
+                padding: const EdgeInsets.only(right: 30, left: 30),
+                alignment: Alignment.center,
+                child: ListView(
+                    shrinkWrap: true,
+                    scrollDirection: Axis.vertical,
+                    children: <Widget>[
+                      Text("Sign in",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: 40,
+                            fontWeight: FontWeight.bold,
+                          )),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Text("Email", style: TextStyle(fontSize: 20)),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      TextField(
+                        //controller: username,
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'Email...',
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Text("Password", style: TextStyle(fontSize: 20)),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      TextField(
+                        //controller: password,
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'Password...',
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      ElevatedButton(
+                        onPressed: (() {
+                          debugPrint("Sign in!");
+                          //ลิงค์ไปหน้า Menu ตรงนี้
+                          getData();
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const Loginpage()),
+                          );
+                        }),
+                        child: Text("Sign in"),
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.blueAccent,
+                            padding: EdgeInsets.all(20)),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      TextButton(
+                        onPressed: (() {
+                          debugPrint("Go to sign up!");
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => Signup()),
+                          );
+                        }),
+                        child: Text("Sign up",
+                            style: TextStyle(
+                              color: Color.fromARGB(255, 0, 46, 84),
+                            )),
+                      )
+                    ]),
+              ))
         ],
-    )),
+      )),
     );
   }
 }
